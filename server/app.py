63e1d8042997a4a42f18d940ad461a27380b5027
Abhishek Jaiswal <abhicse003@gmai.com>
2026-03-29 02:05:14 +0530

    changes has been done in app.py

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from server.environment import MyEnvironment
+from fastapi import Request
 
 app = FastAPI()
 env = MyEnvironment()
@@ -9,12 +10,17 @@ class Action(BaseModel):
     action_type: str
     content: str | None = None
 
+
 @app.api_route("/reset", methods=["GET", "POST"])
-def reset():
+def reset(request: Request):
+    print("METHOD:", request.method)
+
     obs = env.reset()
+
     return {
         "observation": obs
     }
+
 @app.post("/step")
 def step(action: dict):
     obs, reward, done, info = env.step(action)
